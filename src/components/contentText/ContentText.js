0449e9ff5ab7200fccaa0977fb8cf973bdb0e428
Matias Peña <128076299+peniamati@users.noreply.github.com>
2024-05-09 21:04:03 -0300

    Add files via upload

---
+++ src/components/contentText/ContentText.js
@@ -0,0 +1,24 @@
+export const ABOUT_TEXT = {
+  title: "Sobre Nosotros",
+  content: "Bahia Mobility es líder en soluciones de movilidad urbana sostenible. Nuestra empresa está comprometida con la innovación y la excelencia, ofreciendo productos y servicios de calidad que promueven un futuro más limpio y eficiente. Explora nuestra amplia gama de soluciones y descubre cómo estamos transformando la manera en que nos movemos en las ciudades. Desde vehículos eléctricos hasta sistemas de transporte inteligente, en Bahia Mobility estamos dedicados a crear un entorno urbano más accesible, seguro y respetuoso con el medio ambiente. Únete a nosotros en nuestro compromiso con la movilidad sostenible y haz parte del cambio hacia un futuro mejor.",
+  image: "https://i.ibb.co/5szXyHg/camiones.webp",
+  historyContent: "En el corazón de nuestra historia yace un espíritu pionero que se remonta a décadas atrás. Todo comenzó en los vibrantes años 80, cuando un grupo de visionarios se unió con un propósito común: transformar la movilidad urbana para siempre. Desde nuestros humildes inicios como un taller de reparación de bicicletas hasta convertirnos en un referente global en soluciones de movilidad sostenible, nuestro viaje ha sido marcado por la innovación y el compromiso con la excelencia. A lo largo de los años, hemos superado desafíos, celebrado triunfos y aprendido lecciones valiosas que han forjado el camino hacia el futuro. Hoy, mientras reflexionamos sobre nuestra historia, miramos hacia adelante con entusiasmo, listos para continuar nuestro legado de transformación y liderazgo en la industria de la movilidad.",
+  historyImage: "https://fotonesmicamion.com/wp-content/uploads/2023/07/historia-nosotros.jpg.webp"
+};
+
+export const HOME_TEXT = {
+  content: "Bahía Mobility, es una empresa bahiense con más de 10 años en el mercado del transporte, dedicada a comercializar camiones de excelente calidad, configurados especialmente para nuestras carreteras. Tenemos un equipo de colaboradores altamente calificados que brinda soluciones con la rapidez que nuestros clientes requieren."
+}
+
+export const AFTER_SELL_TEXT = {
+  title: "Post Venta",
+  content: "El equipo de Bahia Mobility se enorgullece de ofrecer soluciones especializadas y eficientes a tus necesidades vehiculares. Nuestro personal altamente capacitado y certificado garantiza un servicio de primera clase, cumpliendo con los estándares más exigentes de la industria. Como taller certificado por Cummins a nivel nacional, contamos con un arsenal de herramientas especializadas y un amplio stock de repuestos genuinos, asegurando así que cada intervención se realice con la calidad y la rapidez que mereces.",
+  content2: "Entendemos plenamente la importancia de cada minuto en tus operaciones, por eso nos comprometemos a minimizar cualquier tiempo de inactividad. Nuestra eficiencia se refleja en nuestros tiempos de entrega reducidos, lo que te permite volver a la carretera con la menor interrupción posible. En Bahia Mobility, no solo nos esforzamos por cumplir con tus expectativas, sino por superarlas en cada paso del camino. Confía en nosotros para mantener tus vehículos en óptimas condiciones y maximizar su rendimiento en todo momento.",
+  buttonText: "Solicitar Turno"
+};
+
+export const USEDS_TEXT = {
+  title: "Usados",
+  description: "En nuestra búsqueda por ofrecerte solo lo mejor, cada vehículo en nuestra sección de usados ha sido sometido a rigurosos controles de calidad y mantenimiento. Desde camiones ligeros hasta pesados, cada unidad ha sido escogida meticulosamente para garantizar su calidad, desempeño y confiabilidad.",
+  description2: "Nuestro equipo de expertos realiza exhaustivas inspecciones y pruebas para asegurarse de que cada vehículo cumpla con nuestros exigentes estándares. Nos comprometemos a ofrecerte una amplia variedad de opciones de alta calidad que se adapten a tus necesidades y presupuesto. Ya sea que estés buscando un camión para tu negocio o para tu uso personal, puedes confiar en que encontrarás solo los mejores vehículos en nuestra sección de usados. Además, nuestro equipo de ventas está aquí para brindarte toda la asistencia y orientación que necesites para tomar la mejor decisión. Explora nuestra selección de camiones usados hoy mismo y descubre la calidad y confiabilidad que solo nuestra concesionaria puede ofrecerte."
+};
